cbbbd99d0302e517054e377df24b8966b02ab8a5
liondor <johen.sylvestre@gmail.com>
2020-01-22 11:29:05 -0400

    Ajoute une WildCard
    J'ai ajouté une wildCard dans la route pour commencer à réfléchir sur comment gérer le ticket

--- web/modules/custom/hello_world/src/Controller/HelloWorldController.php
+++ web/modules/custom/hello_world/src/Controller/HelloWorldController.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 
 namespace Drupal\hello_world\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 
 class HelloWorldController{
-  public function sayHello()
+  public function sayHello($ticket)
   {
-    return new Response('Hello');
+    return new Response('Hello, your ticket is'+$ticket);
   }
 }
 
