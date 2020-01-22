86ef0a2c6fc20fbae27956b4b89e35d570b8ee60
liondor <johen.sylvestre@gmail.com>
2020-01-22 11:33:51 -0400

    Change l'opérateur pour la concaténation
    En php, on utilise un point pour effectuer une concaténation, et non pas l'opérateur + comme en JS

--- web/modules/custom/hello_world/src/Controller/HelloWorldController.php
+++ web/modules/custom/hello_world/src/Controller/HelloWorldController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 class HelloWorldController{
   public function sayHello($ticket)
   {
-    return new Response('Hello, your ticket is'+$ticket);
+    return new Response('Hello, your ticket is'.$ticket);
   }
 }
 
