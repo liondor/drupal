6100c1f55736d810e979cccda1c42c32c88cfa55
liondor <johen.sylvestre@gmail.com>
2020-01-22 12:15:29 -0400

    Récupère le résultat d'une recherche contenu
    Toujours pour tester les possibilités qui nous sont ouvertes avec les modules, j'effectue un test avec la fonction file_get_contents

--- web/modules/custom/hello_world/src/Controller/HelloWorldController.php
+++ web/modules/custom/hello_world/src/Controller/HelloWorldController.php
@@ -7,7 +7,18 @@ use Symfony\Component\HttpFoundation\Response;
 class HelloWorldController{
   public function sayHello($ticket)
   {
-    return new Response('Hello, your ticket is :'.$ticket);
+//The URL with parameters / query string.
+$url = 'http://google.com/search?q='.$ticket;
+
+//Once again, we use file_get_contents to GET the URL in question.
+$contents = file_get_contents($url);
+
+//If $contents is not a boolean FALSE value.
+if($contents !== false){
+  //Print out the contents.
+  echo $contents;
+}
+    return new Response('Hello, your ticket is :'.$contents);
   }
 }
 
