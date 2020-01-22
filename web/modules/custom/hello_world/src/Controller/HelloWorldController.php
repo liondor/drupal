b556113b98764405e9a46ee9dca75c9831dd1ec6
liondor <johen.sylvestre@gmail.com>
2020-01-22 11:48:32 -0400

    Change la route du module hello_world
    J'ai modifier la route du module hello_world afin de simuler le traitement de requêtes GET via mon module

--- web/modules/custom/hello_world/src/Controller/HelloWorldController.php
+++ web/modules/custom/hello_world/src/Controller/HelloWorldController.php
@@ -5,9 +5,9 @@ namespace Drupal\hello_world\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 class HelloWorldController{
-  public function sayHello($ticket)
+  public function sayHello($ticket, $id)
   {
-    return new Response('Hello, your ticket is'.$ticket);
+    return new Response('Hello, your ticket is :'.$ticket.' and your id is : '.$id);
   }
 }
 
